ae4728d7585f0f8071f0283c93dbc21460a2ff8c
howardbdev <howarddevennish@gmail.com>
2019-05-31 17:59:21 -0400

    remove componentdidmount

--- src/App.js
+++ src/App.js
@@ -3,11 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends React.Component {
-  componentDidMount(){
-    fetch("http://localhost:3000/api/v1/users/1")
-      .then(r=>r.json())
-      .then(console.log)
-  }
+
   render(){
     return (
       <div className="App">
