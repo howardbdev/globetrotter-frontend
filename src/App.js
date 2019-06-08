3541a64755e0da3efc341a79b5f44dcf03e13d2c
howardbdev <howarddevennish@gmail.com>
2019-06-07 23:19:41 -0400

    add componentDidMount to get current user on load if there is one

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import './App.css';
+import Login from "./components/Login.js"
+import { connect } from 'react-redux'
+import { getCurrentUser } from "./actions/currentUser.js"
 
 class App extends React.Component {
 
+  componentDidMount() {
+    this.props.getCurrentUser()
+  }
+
   render(){
     return (
-      "hello I'm React"
+      <Login/>
     );
 
   }
 }
 
-export default App;
+export default connect(null, { getCurrentUser })(App);
