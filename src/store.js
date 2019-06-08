6d8e1e26260e142ffc3202e7f517862aa4884ba5
howardbdev <howarddevennish@gmail.com>
2019-06-07 23:19:13 -0400

    add new reducers to combineReducers

--- src/store.js
+++ src/store.js
@@ -1,10 +1,14 @@
 import { createStore, applyMiddleware, compose, combineReducers } from 'redux'
 import usersReducer from './reducers/users.js'
+import currentUser from './reducers/currentUser.js'
+import loginForm from './reducers/loginForm.js'
 import thunk from 'redux-thunk'
 
-
+// just displaying different syntax options here (lines 8 and 9)
 const reducer = combineReducers({
-  users: usersReducer
+  users: usersReducer,
+  currentUser,
+  loginForm
 })
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
