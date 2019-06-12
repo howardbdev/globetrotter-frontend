b497d12b55d7d6ba4d19bee0d0bcc354376ba35b
howardbdev <howarddevennish@gmail.com>
2019-06-12 07:30:44 -0400

    start to split up components with nav and main container

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import './App.css';
-import Login from "./components/Login.js"
-import Logout from "./components/Logout.js"
 import { connect } from 'react-redux'
 import { getCurrentUser } from "./actions/currentUser.js"
+import NavBar from './components/NavBar.js'
+import MainContainer from './components/MainContainer.js'
 
 class App extends React.Component {
 
@@ -13,22 +13,15 @@ class App extends React.Component {
 
   render(){
     return (
-      // thnk about refactoring this to a nav bar 
-      this.props.currentUser ? <Logout/> : <Login/>
+      <div className="App">
+        <NavBar/>
+        <MainContainer/>
+      </div>
     );
 
   }
 }
 
-// I an do this because I know the incoming argument is an object, state, coming from redux
-// and I know it has a property called currentUser
-// state = { ...,
-//   currentUser: {...}
-// }
-const mapStateToProps = ({ currentUser }) => {
-  return {
-    currentUser
-  }
-}
 
-export default connect(mapStateToProps, { getCurrentUser })(App);
+
+export default connect(null, { getCurrentUser })(App);
