86238ccdbe086d6c72e935ca9af9739794eb365c
howardbdev <howarddevennish@gmail.com>
2019-06-14 11:11:19 -0400

    add reset login form action

--- src/reducers/loginForm.js
+++ src/reducers/loginForm.js
@@ -7,6 +7,8 @@ export default (state = initialState, action) => {
   switch (action.type) {
     case "UPDATE_LOGIN_FORM":
       return action.formData
+    case "RESET_LOGIN_FORM":
+      return initialState
     default:
       return state
   }
