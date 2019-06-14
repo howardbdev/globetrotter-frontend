3b7cdb91038fe6c3043000bfb11a0d248e1c0278
howardbdev <howarddevennish@gmail.com>
2019-06-14 11:11:39 -0400

    add myTrips to store

--- src/store.js
+++ src/store.js
@@ -2,13 +2,15 @@ import { createStore, applyMiddleware, compose, combineReducers } from 'redux'
 import usersReducer from './reducers/users.js'
 import currentUser from './reducers/currentUser.js'
 import loginForm from './reducers/loginForm.js'
+import myTrips from './reducers/myTrips.js'
 import thunk from 'redux-thunk'
 
 // just displaying different syntax options here (lines 8 and 9)
 const reducer = combineReducers({
   users: usersReducer,
   currentUser,
-  loginForm
+  loginForm,
+  myTrips
 })
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
