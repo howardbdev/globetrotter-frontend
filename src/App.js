bfff17daa6e8bf08e9f12c5d411c8963d30156b2
howardbdev <howarddevennish@gmail.com>
2019-06-18 19:45:46 -0400

    add routes to App

--- src/App.js
+++ src/App.js
@@ -3,7 +3,12 @@ import './App.css';
 import { connect } from 'react-redux'
 import { getCurrentUser } from "./actions/currentUser.js"
 import NavBar from './components/NavBar.js'
+import Login from './components/Login.js'
+import Logout from './components/Logout.js'
+import Signup from './components/Signup.js'
+import MyTrips from './components/MyTrips.js'
 import MainContainer from './components/MainContainer.js'
+import { Route } from 'react-router-dom'
 
 class App extends React.Component {
 
@@ -14,8 +19,11 @@ class App extends React.Component {
   render(){
     return (
       <div className="App">
-        <NavBar/>
-        <MainContainer/>
+      <Logout/>
+      <NavBar/>
+        <Route exact path='/signup' component={Signup}/>
+        <Route exact path='/login' component={Login}/>
+        <Route exact path='/my-trips' component={MyTrips}/>
       </div>
     );
 
