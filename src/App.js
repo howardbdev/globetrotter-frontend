643a04848eb84d6408a1cf4bcf79b7e35715435b
howardbdev <howarddevennish@gmail.com>
2019-06-26 07:47:43 -0400

    add Home and some routes to App

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import { connect } from 'react-redux'
 import { getCurrentUser } from "./actions/currentUser.js"
 import NavBar from './components/NavBar.js'
+import Home from './components/Home.js'
 import Login from './components/Login.js'
 import Logout from './components/Logout.js'
 import Signup from './components/Signup.js'
 import MyTrips from './components/MyTrips.js'
+import NewTripForm from './components/NewTripForm.js'
 import MainContainer from './components/MainContainer.js'
-import { Route } from 'react-router-dom'
+import { Route, Switch, withRouter } from 'react-router-dom'
 
 class App extends React.Component {
 
@@ -17,19 +19,25 @@ class App extends React.Component {
   }
 
   render(){
+    const { loggedIn } = this.props
     return (
       <div className="App">
-      <Logout/>
-      <NavBar/>
-        <Route exact path='/signup' component={Signup}/>
+        { loggedIn ? <Logout/> : null}
+        <Route exact path='/signup' render={({history})=><Signup history={history}/>}/>
         <Route exact path='/login' component={Login}/>
-        <Route exact path='/my-trips' component={MyTrips}/>
+        <Route exact path='/' render={(props)=> loggedIn ? <MyTrips {...props}/> : <Home {...props}/>}/>
+        <Route exact path='/trips' component={MyTrips}/>
+        <Route exact path='/trips/new' component={NewTripForm}/>
       </div>
     );
 
   }
 }
 
+const mapStateToProps = state => {
+  return ({
+    loggedIn: !!state.currentUser
+  })
+}
 
-
-export default connect(null, { getCurrentUser })(App);
+export default withRouter(connect(mapStateToProps, { getCurrentUser })(App));
