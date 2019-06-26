2ef741f03831c6301d05d58296fde8d42de49f80
howardbdev <howarddevennish@gmail.com>
2019-06-26 07:50:18 -0400

    update nav bar - removing login and signup

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,16 +1,11 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import Login from "./Login.js"
-import Logout from "./Logout.js"
 
 //
 const NavBar = ({ currentUser }) => {
   return (
     <div className="NavBar">
       { currentUser ? <strong>Welcome, {currentUser.attributes.name} from {currentUser.attributes.hometown.city}</strong> : "" }
-      <button> Log In </button>
-      OR
-      <button> Sign Up </button>
     </div>
   )
 }
