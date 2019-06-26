0320224bbbf638feb704c32226b645fe094a2f83
howardbdev <howarddevennish@gmail.com>
2019-06-26 07:51:10 -0400

    change 'as Router' to just BrowserRouter - doesn't matter just for demo

--- src/index.js
+++ src/index.js
@@ -5,10 +5,10 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { Provider } from 'react-redux'
 import store from './store.js'
-import { BrowserRouter as Router } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 
 
-ReactDOM.render(<Provider store={ store }><Router><App /></Router></Provider>, document.getElementById('root'));
+ReactDOM.render(<Provider store={ store }><BrowserRouter><App /></BrowserRouter></Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
