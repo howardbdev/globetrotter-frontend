df71ff1bcd5fda5a2f26edab391eb803171878c7
howardbdev <howarddevennish@gmail.com>
2019-06-26 07:51:36 -0400

    add newTripForm to store

--- src/store.js
+++ src/store.js
@@ -4,6 +4,7 @@ import currentUser from './reducers/currentUser'
 import loginForm from './reducers/loginForm'
 import myTrips from './reducers/myTrips'
 import signupForm from './reducers/signupForm'
+import newTripForm from './reducers/newTripForm'
 import thunk from 'redux-thunk'
 
 // just displaying different syntax options here (lines 8 and 9)
@@ -12,7 +13,8 @@ const reducer = combineReducers({
   currentUser,
   loginForm,
   myTrips,
-  signupForm
+  signupForm,
+  newTripForm
 })
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
