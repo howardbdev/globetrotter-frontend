0825b243c6c0787315c3ca5de3b6c047547a33cf
howardbdev <howarddevennish@gmail.com>
2019-06-28 10:17:53 -0400

    add preventDefault and a re-route on logout - used withRouter to get history

--- src/components/Logout.js
+++ src/components/Logout.js
@@ -1,17 +1,23 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { logout } from "../actions/currentUser.js"
+import { withRouter } from 'react-router-dom'
 
 
-const Logout = ({ logout }) => {
+const Logout = ({ logout, history }) => {
   return (
-    <form onSubmit={logout}>
+    <form onSubmit={(event) => {
+        event.preventDefault()
+        logout()
+        history.push('/')
+      }
+    }>
       <input type="submit" value="Log Out"/>
     </form>
   )
 }
 
-export default connect(null, { logout } )(Logout)
+export default withRouter(connect(null, { logout } )(Logout))
 
 // I still need....
 
