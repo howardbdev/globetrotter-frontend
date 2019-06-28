5ca3b37930f22e77be1b3f615715da6ae6aefa8f
howardbdev <howarddevennish@gmail.com>
2019-06-28 10:20:43 -0400

    remove users from store

--- src/store.js
+++ src/store.js
@@ -1,5 +1,5 @@
 import { createStore, applyMiddleware, compose, combineReducers } from 'redux'
-import usersReducer from './reducers/users'
+// import usersReducer from './reducers/users'
 import currentUser from './reducers/currentUser'
 import loginForm from './reducers/loginForm'
 import myTrips from './reducers/myTrips'
@@ -9,7 +9,6 @@ import thunk from 'redux-thunk'
 
 // just displaying different syntax options here (lines 8 and 9)
 const reducer = combineReducers({
-  users: usersReducer,
   currentUser,
   loginForm,
   myTrips,
