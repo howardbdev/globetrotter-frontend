688c1bbc0ee34e0c2ebf3454eba16e7a5ca9ea27
howardbdev <howarddevennish@gmail.com>
2019-06-28 21:52:41 -0400

    add ADD_TRIP case in myTrips reducer

--- src/reducers/myTrips.js
+++ src/reducers/myTrips.js
@@ -4,6 +4,8 @@ export default (state = initialState, action) => {
   switch (action.type) {
     case "SET_MY_TRIPS":
       return action.trips
+    case "ADD_TRIP":
+      return state.trips.concat(action.trips)
     case "CLEAR_TRIPS":
       return initialState
     default:
