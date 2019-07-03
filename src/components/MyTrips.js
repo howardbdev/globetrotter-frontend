75c2aac07685bb592cdfd2f0c4925e67decd8a03
howardbdev <howarddevennish@gmail.com>
2019-07-02 23:08:42 -0400

    change trips index to links

--- src/components/MyTrips.js
+++ src/components/MyTrips.js
@@ -1,15 +1,13 @@
 import React from 'react'
-import TripCard from './TripCard.js'
 import { connect } from 'react-redux'
+import { Link } from 'react-router-dom'
 
 const MyTrips = props => {
   const tripCards = props.trips.length > 0 ?
-    props.trips.map(t => <TripCard trip={t} key={t.id}/>) :
+    props.trips.map(t => (<><Link key={t.id} to={`/trips/${t.id}`}>{t.attributes.name}</Link><br/></>)) :
     null
 
-  return (
-    tripCards
-  )
+  return tripCards
 }
 
 // we provide mapStateToProps to Redux in order to tell Redux:
