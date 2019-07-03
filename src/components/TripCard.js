416fc9e421f62eef0be2752247a596aa659982b1
howardbdev <howarddevennish@gmail.com>
2019-07-02 23:10:18 -0400

    add more details and link to edit to TripCard

--- src/components/TripCard.js
+++ src/components/TripCard.js
@@ -1,8 +1,16 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
 const TripCard = ({ trip }) => {
   return (
-    <p>{trip.attributes.name}</p>
+    trip ?
+      <div>
+        <h3>{trip.attributes.name}</h3>
+        <p>{trip.attributes.start_date}</p>
+        <p>{trip.attributes.end_date}</p>
+        <Link to={`/trips/${trip.id}/edit`}>Edit this trip</Link>
+      </div> :
+      <p>This the the Trip card with no trip!</p>
   )
 }
 
