d99a6f946862a45f7c07ab4279ff4088e8bef82f
howardbdev <howarddevennish@gmail.com>
2019-07-03 20:14:39 -0400

    add case for UPDATE_TRIP in myTrips reducer - just a placeholder still wip

--- src/reducers/myTrips.js
+++ src/reducers/myTrips.js
@@ -6,6 +6,9 @@ export default (state = initialState, action) => {
       return action.trips
     case "ADD_TRIP":
       return state.concat(action.trip)
+    case "UPDATE_TRIP":
+      console.log("in UPDATE TRIP action is", action)
+      return state
     case "CLEAR_TRIPS":
       return initialState
     default:
